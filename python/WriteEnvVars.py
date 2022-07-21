e4fc2cb0d1fa521a71363a6e7940b5087bf876fe
Dan <d.gr.smith@gmail.com>
2022-07-21 16:32:53 -0400

    Change to select by env var naming conventions

--- python/WriteEnvVars.py
+++ python/WriteEnvVars.py
@@ -20,15 +20,22 @@ def env_as_dict():
     """
     return os.environ
 
+def env_vars_to_filter():
+    """
+    Return a list of environment variables to filter based on a common naming convention.
+    """
+    naming_convention = 'z_'
+    new_filt = dict(filter(lambda item: item[0].startswith(naming_convention), env_as_dict().items()))
+    return new_filt
+
 def filter_env_vars(env_vars):
     """
     Return a dictionary of environment variables that are not in the list of
     environment variables to filter.
     """
     filtered_env_vars = {}
-    env_vars_to_filter = ['z_trial']
     for key in env_vars.keys():
-        if key in env_vars_to_filter:
+        if key in env_vars_to_filter():
             filtered_env_vars[key] = env_vars[key]
     return filtered_env_vars
 
